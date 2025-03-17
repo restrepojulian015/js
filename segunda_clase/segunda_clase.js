016ea8814bd66bafce3ce66e72e94ff9d4113962
restrepojulian015 <restrepojulian015@gmail.com>
2025-03-17 13:38:57 -0500

    second commit

---
+++ segunda_clase/segunda_clase.js
@@ -0,0 +1,64 @@
+// x = prompt("Digite el primer número: ")
+// y = prompt("Digite el Segundo número: ")
+
+// if (x > y ){
+//     alert("El primer número es mayor")
+// }else if(x == y ){
+//     alert("son iguales número es mayor")
+// }else{
+//     alert("El segundo número es mayor")
+// }
+
+// var saldo = 100;
+
+// const monto = parseInt(prompt("Digite monto a retirar"));
+// const esMenor = (saldo >= monto) ? true:false;
+// alert(esMenor ? "Transacción permitida":"Saldo insuficiente");
+
+// alert(`Saldo actual: ${saldo -= monto} pesos`);
+
+// let dia = prompt("DIme el día: ")
+
+
+// switch (dia){
+//     case "lunes":
+//         alert("Comienzo de Semana");
+//         break;
+//     case "viernes":
+//         alert("Fin de semana");
+//         break;
+//     default:
+//         alert("Día intermedio");
+// }
+
+// const arreglo = [1,70,"Naranja", false];
+
+// for ( let i = 0; i <arreglo.length;i++){
+//     console.log(arreglo[i]);
+// }
+// const arreglo = [1,70,"Naranja", false];
+
+// for (const item of arreglo){
+//     console.log(item);
+// }
+
+// for (const index in arreglo){
+//     console.log(`En la posiciòn ${index}, el valor es ${arreglo[index]}`)
+// }
+
+// const obj = {nombre: "juan", apellido : "mendez", edad: 200, vago: true}
+// for (const clave in obj){
+//     console.log(`clave: ${clave}, valor: ${obj[clave]}`);
+// }
+
+// let i = 0;
+
+// while(i<10){
+//     console.log(i++);
+// }
+
+let x = 0;
+
+do{
+ x = parseInt(prompt("digite número"));
+}while(x == 10)
